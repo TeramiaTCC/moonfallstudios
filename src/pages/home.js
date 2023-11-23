141544e71e4d9fc4585cb36a9d2c02d17b116b9b
TERAMIA-TCC <maldosostcc@gmail.com>
2023-11-23 10:01:28 -0300

    scroll

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,26 +1,25 @@
 import '../css/styles.css';
+import '../css/scroll.css';
+import Moon from '../assets/images/Moon.png'
 
 function Home() {
   return (
     <div class='container'> 
         <header>
             <nav> 
-                <div class="logo"> 
-                    <img src= "https://placehold.co/600x100" alt='logo' /> 
-                </div> 
-                <ul class="nav-list"> 
+                <img src= "https://placehold.co/600x100" alt='logo' /> 
+                <ul class="list"> 
                     <li><a href="#Projetos">Projetos</a></li> 
                     <li><a href="#Produções">Produções</a></li> 
                     <li><a href="#Sobre">Sobre nós</a></li> 
                     <li><a href='#Midias'>Midias</a></li> 
                 </ul> 
-
             </nav>
         </header>
 
         <main>
             <section > 
-                <img src="../asset/images/Moon.png" alt='moon'/>
+                <img src={Moon} alt='moon' height={600}/>
             </section> 
         </main>
 
